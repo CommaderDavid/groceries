21a1c7c84e0545ca7411b3dd2fc5342a5a62bdd4
David Boedigheimer <stealth259@gmail.com>
2019-10-21 15:24:12 -0700

    Add change to form and make a one list array.

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,11 @@
 // front end
 $(document).ready(function() {
   $("form#market").submit(function(e) {
-    var foods = ["food1", "food2", "food3", "food4", "food5", "food6"]
-
-    foods.forEach(function(food) {
-      var userInput = $("input#" + food).val();
-      $(food).text(userInput);
-      $('ol#list').prepend("<li>" + food + "</li>");
+    var gorc = [];
+    var list = $("#groc").val().split(",").sort();
+    
+    list.forEach(function(item) {
+      $("ul#list").append("<li>" + item.toUpperCase() + "</li>");
     });
 
     $("#market").hide();
